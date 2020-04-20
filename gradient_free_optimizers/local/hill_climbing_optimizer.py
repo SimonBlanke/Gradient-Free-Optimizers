31249d48395eda5a8d4c6b19b345a16cdfa1a2f1
SimonBlanke <simonblanke@hotmail.de>
2020-04-20 18:34:18 +0200

    change to new api

--- gradient_free_optimizers/local/hill_climbing_optimizer.py
+++ gradient_free_optimizers/local/hill_climbing_optimizer.py
@@ -7,11 +7,15 @@ from ..base_positioner import BasePositioner
 
 
 class HillClimbingOptimizer(BaseOptimizer):
-    def __init__(self, space_dim, opt_para):
-        super().__init__(space_dim, opt_para)
+    def __init__(self, init_positions, space_dim, opt_para):
+        super().__init__(init_positions, space_dim, opt_para)
 
-    def init_pos(self, init_position):
-        self._base_init_pos(init_position, HillClimbingPositioner)
+    def init_pos(self, nth_init):
+        pos_new = self._base_init_pos(
+            nth_init, HillClimbingPositioner(self.space_dim, self._opt_args_)
+        )
+
+        return pos_new
 
     def iterate(self, nth_iter):
         self._base_iterate(nth_iter)
@@ -20,13 +24,11 @@ class HillClimbingOptimizer(BaseOptimizer):
         return pos
 
     def evaluate(self, score_new):
-        self._base_evaluate(score_new)
+        self._evaluate_new2best(score_new)
 
         if self.nth_iter % self._opt_args_.n_neighbours == 0:
             self.p_current.score_current = self.p_current.score_best
             self.p_current.pos_current = self.p_current.pos_best
-
-        self.nth_iter += 1
 
 
 class HillClimbingPositioner(BasePositioner):
