8e2303a5dfded691d5ec1fc4eb7f8038d9e0f610
SimonBlanke <simonblanke@hotmail.de>
2020-04-28 10:06:15 +0200

    change test to new api

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -4,184 +4,111 @@
 
 import numpy as np
 
-from hyperactive import Hyperactive
+from gradient_free_optimizers import (
+    HillClimbingOptimizer,
+    StochasticHillClimbingOptimizer,
+    TabuOptimizer,
+    RandomSearchOptimizer,
+    RandomRestartHillClimbingOptimizer,
+    RandomAnnealingOptimizer,
+    SimulatedAnnealingOptimizer,
+    StochasticTunnelingOptimizer,
+    ParallelTemperingOptimizer,
+    ParticleSwarmOptimizer,
+    EvolutionStrategyOptimizer,
+    BayesianOptimizer,
+    TreeStructuredParzenEstimators,
+    DecisionTreeOptimizer,
+)
 
-X, y = np.array([0]), np.array([0])
-memory = False
 n_iter = 100
 
 
-def sphere_function(para, X_train, y_train):
-    loss = []
-    for key in para.keys():
-        if key == "iteration":
-            continue
-        loss.append(para[key] * para[key])
-
-    return -np.array(loss).sum()
+def get_score(pos_new):
+    return -(pos_new[0] * pos_new[0])
 
 
-search_config = {
-    sphere_function: {
-        "x1": list(np.arange(-3, 3, 0.1)),
-        "x2": list(np.arange(-3, 3, 0.1)),
-    }
-}
+space_dim = np.array([10])
+init_positions = [np.array([0]), np.array([1]), np.array([2]), np.array([3])]
+
+
+def _base_test(opt):
+    for nth_init in range(len(init_positions)):
+        pos_new = opt.init_pos(nth_init)
+        score_new = get_score(pos_new)
+        opt.evaluate(score_new)
+
+    for nth_iter in range(len(init_positions), n_iter):
+        pos_new = opt.iterate(nth_iter)
+        score_new = get_score(pos_new)
+        opt.evaluate(score_new)
 
 
 def test_HillClimbingOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="HillClimbing")
-
-    for epsilon in [0.01, 0.1, 1]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"HillClimbing": {"epsilon": epsilon}},
-        )
+    opt = HillClimbingOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_StochasticHillClimbingOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="StochasticHillClimbing")
-
-    for p_down in [0.01, 0.1, 1]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"StochasticHillClimbing": {"p_down": p_down}},
-        )
+    opt = StochasticHillClimbingOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_TabuOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="TabuSearch")
-
-    for tabu_memory in [1, 3, 5]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"TabuSearch": {"tabu_memory": tabu_memory}},
-        )
+    opt = TabuOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_RandomSearchOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="RandomSearch")
+    opt = RandomSearchOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_RandomRestartHillClimbingOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="RandomRestartHillClimbing")
-
-    for n_restarts in [3, 5, 20]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"RandomRestartHillClimbing": {"n_restarts": n_restarts}},
-        )
+    opt = RandomRestartHillClimbingOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_RandomAnnealingOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="RandomAnnealing")
-
-    for start_temp in [0.1, 1, 10]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"RandomAnnealing": {"start_temp": start_temp}},
-        )
+    opt = RandomAnnealingOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_SimulatedAnnealingOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="SimulatedAnnealing")
-
-    for start_temp in [0.1, 1, 10]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"SimulatedAnnealing": {"start_temp": start_temp}},
-        )
+    opt = SimulatedAnnealingOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_StochasticTunnelingOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="StochasticTunneling")
-
-    for start_temp in [0.1, 1, 10]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"StochasticTunneling": {"start_temp": start_temp}},
-        )
+    opt = StochasticTunnelingOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_ParallelTemperingOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="ParallelTempering")
-
-    for n_swaps in [1, 10, 30]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"ParallelTempering": {"n_swaps": n_swaps}},
-        )
+    opt = ParallelTemperingOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_ParticleSwarmOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="ParticleSwarm")
-
-    for n_particles in [2, 10, 30]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"ParticleSwarm": {"n_particles": n_particles}},
-        )
+    opt = ParticleSwarmOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_EvolutionStrategyOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=n_iter, optimizer="EvolutionStrategy")
-
-    for individuals in [2, 10, 30]:
-        opt = Hyperactive(X, y, memory=memory)
-        opt.search(
-            search_config,
-            n_iter=n_iter,
-            optimizer={"EvolutionStrategy": {"individuals": individuals}},
-        )
+    opt = EvolutionStrategyOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_BayesianOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=int(n_iter / 33), optimizer="Bayesian")
-
-    for warm_start_smbo in [True]:
-        opt = Hyperactive(X, y, memory="long")
-        opt.search(
-            search_config,
-            n_iter=int(n_iter / 33),
-            optimizer={"Bayesian": {"warm_start_smbo": warm_start_smbo}},
-        )
+    opt = BayesianOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
-def test_TPE():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=int(n_iter / 5), optimizer="TPE")
+def test_TreeStructuredParzenEstimators():
+    opt = TreeStructuredParzenEstimators(init_positions, space_dim, opt_para={})
+    _base_test(opt)
 
 
 def test_DecisionTreeOptimizer():
-    opt = Hyperactive(X, y, memory=memory)
-    opt.search(search_config, n_iter=int(n_iter / 33), optimizer="DecisionTree")
+    opt = DecisionTreeOptimizer(init_positions, space_dim, opt_para={})
+    _base_test(opt)
