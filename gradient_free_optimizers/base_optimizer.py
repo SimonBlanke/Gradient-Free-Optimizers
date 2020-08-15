ff8d0866b5e797e462542e6517eafc8cd610e441
SimonBlanke <simonblanke@hotmail.de>
2020-08-15 20:47:20 +0200

    use search tracker class to save scores and positions during optimization

--- gradient_free_optimizers/base_optimizer.py
+++ gradient_free_optimizers/base_optimizer.py
@@ -5,24 +5,23 @@
 import numpy as np
 from .opt_args import Arguments
 
+from .search_tracker import SearchTracker
 
-class BaseOptimizer:
-    def __init__(self, init_positions, space_dim, opt_para):
-        self._opt_args_ = Arguments(**opt_para)
-        self._opt_args_.set_opt_args()
 
-        self.init_positions = init_positions
+class BaseOptimizer(SearchTracker):
+    def __init__(self, space_dim):
         self.space_dim = space_dim
-        self.opt_para = opt_para
 
-        self.nth_iter = 0
-        self.p_list = []
+        self.init_positions = []
 
+    """
     def _base_init_pos(self, nth_init, positioner):
         init_position = self.init_positions[nth_init]
 
         self.p_current = positioner
         self.p_current.pos_new = init_position
+
+        # print("init_position", init_position)
 
         self.p_list.append(self.p_current)
 
@@ -41,27 +40,34 @@ class BaseOptimizer:
 
         self.p_sorted = [self.p_list[i] for i in idx_sorted_ind]
 
-    def _base_iterate(self, nth_iter):
-        self.nth_iter = nth_iter
-
     def _choose_next_pos(self):
         self.p_current = self.p_list[self.nth_iter % len(self.p_list)]
         self.p_rest = [p for p in self.p_list if p is not self.p_current]
+    """
+
+    def _base_iterate(self, nth_iter):
+        self.nth_iter = nth_iter
 
     def _evaluate_new2current(self, score_new):
-        if score_new >= self.p_current.score_current:
-            self.p_current.score_current = score_new
-            self.p_current.pos_current = self.p_current.pos_new
+        if score_new >= self.score_current:
+            self.score_current = score_new
+            self.pos_current = self.pos_new
 
     def _evaluate_current2best(self):
-        if self.p_current.score_current >= self.p_current.score_best:
-            self.p_current.score_best = self.p_current.score_current
-            self.p_current.pos_best = self.p_current.pos_current
+        if self.score_current >= self.score_best:
+            self.score_best = self.score_current
+            self.pos_best = self.pos_current
 
     def _current2best(self):
-        self.p_current.score_best = self.p_current.score_current
-        self.p_current.pos_best = self.p_current.pos_current
+        self.score_best = self.score_current
+        self.pos_best = self.pos_current
 
     def _new2current(self):
-        self.p_current.score_current = self.p_current.score_new
-        self.p_current.pos_current = self.p_current.pos_new
+        self.score_current = self.score_new
+        self.pos_current = self.pos_new
+
+    def move_random(self):
+        pos_new = np.random.uniform(
+            np.zeros(self.space_dim.shape), self.space_dim, self.space_dim.shape
+        )
+        return np.rint(pos_new).astype(int)
---
+++ gradient_free_optimizers/search_tracker.py
@@ -0,0 +1,86 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+import numpy as np
+
+
+class SearchTracker:
+    def __init__(self):
+        self._pos_new = None
+        self._score_new = -np.inf
+
+        self._pos_current = None
+        self._score_current = -np.inf
+
+        self._pos_best = None
+        self._score_best = -np.inf
+
+        self.pos_new_list = []
+        self.score_new_list = []
+
+        self.pos_current_list = []
+        self.score_current_list = []
+
+        self.pos_best_list = []
+        self.score_best_list = []
+
+    ### new ###
+
+    @property
+    def pos_new(self):
+        return self._pos_new
+
+    @pos_new.setter
+    def pos_new(self, pos):
+        self.pos_new_list.append(pos)
+        self._pos_new = pos
+
+    @property
+    def score_new(self):
+        return self._score_new
+
+    @score_new.setter
+    def score_new(self, score):
+        self.score_new_list.append(score)
+        self._score_new = score
+
+    ### current ###
+
+    @property
+    def pos_current(self):
+        return self._pos_current
+
+    @pos_current.setter
+    def pos_current(self, pos):
+        self.pos_current_list.append(pos)
+        self._pos_current = pos
+
+    @property
+    def score_current(self):
+        return self._score_current
+
+    @score_current.setter
+    def score_current(self, score):
+        self.score_current_list.append(score)
+        self._score_current = score
+
+    ### best ###
+
+    @property
+    def pos_best(self):
+        return self._pos_best
+
+    @pos_best.setter
+    def pos_best(self, pos):
+        self.pos_best_list.append(pos)
+        self._pos_best = pos
+
+    @property
+    def score_best(self):
+        return self._score_best
+
+    @score_best.setter
+    def score_best(self, score):
+        self.score_best_list.append(score)
+        self._score_best = score
