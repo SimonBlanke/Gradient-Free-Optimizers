9de92b02d3ce5fe1e9d63c14ed266a7ef85a87a5
SimonBlanke <simonblanke@hotmail.de>
2020-08-19 20:42:12 +0200

    change base test to new api

--- tests/_base_test.py
+++ tests/_base_test.py
@@ -5,26 +5,12 @@ def get_score(pos_new):
     return -(pos_new[0] * pos_new[0])
 
 
-space_dim = np.array([10])
-init_positions = [np.array([0]), np.array([1]), np.array([2]), np.array([3])]
+search_space = [np.arange(-100, 100, 1)]
 
 
 def _base_test(
-    opt_class,
-    n_iter,
-    get_score=get_score,
-    space_dim=space_dim,
-    init_positions=init_positions,
-    opt_para={},
+    opt_class, n_iter, get_score=get_score, search_space=search_space, opt_para={},
 ):
-    opt = opt_class(space_dim, **opt_para)
+    opt = opt_class(search_space, **opt_para)
+    opt.search(get_score, n_iter=n_iter)
 
-    for init_position in init_positions:
-        opt.init_pos(init_position)
-        score_new = get_score(init_position)
-        opt.evaluate(score_new)
-
-    for _ in range(len(init_positions), n_iter):
-        pos_new = opt.iterate()
-        score_new = get_score(pos_new)
-        opt.evaluate(score_new)
