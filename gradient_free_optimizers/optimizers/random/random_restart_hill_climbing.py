33bb93ce8f207385cdfb8285e7b5b1277f1fa68f
SimonBlanke <simonblanke@hotmail.de>
2020-08-26 19:52:44 +0200

    fix bug in nth_iter count with number of pos_new

--- gradient_free_optimizers/optimizers/random/random_restart_hill_climbing.py
+++ gradient_free_optimizers/optimizers/random/random_restart_hill_climbing.py
@@ -8,13 +8,13 @@ from ...search import Search
 
 
 class RandomRestartHillClimbingOptimizer(HillClimbingOptimizer, Search):
-    def __init__(self, search_space, n_iter_restart=10, **kwargs):
-        super().__init__(search_space, **kwargs)
+    def __init__(self, search_space, n_iter_restart=10):
+        super().__init__(search_space)
         self.n_iter_restart = n_iter_restart
 
     def iterate(self):
         notZero = self.n_iter_restart != 0
-        modZero = len(self.pos_new) % self.n_iter_restart == 0
+        modZero = len(self.pos_new_list) % self.n_iter_restart == 0
 
         if notZero and modZero:
             pos = self.move_random()
