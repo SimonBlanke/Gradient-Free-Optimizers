3db5ddece198b8030c33c8028d8e3c2d03f03779
SimonBlanke <simonblanke@hotmail.de>
2020-10-25 17:58:14 +0100

    fix bug in random search

--- gradient_free_optimizers/optimizers/random/random_search.py
+++ gradient_free_optimizers/optimizers/random/random_search.py
@@ -11,6 +11,7 @@ class RandomSearchOptimizer(BaseOptimizer, Search):
     def __init__(self, search_space, **kwargs):
         super().__init__(search_space)
 
+    @BaseOptimizer.track_nth_iter
     def iterate(self):
         return self.move_random()
 
