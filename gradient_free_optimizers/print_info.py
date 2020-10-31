fe5e195a8ca2fa604c14dc3c927a3a65313f7d03
SimonBlanke <simonblanke@hotmail.de>
2020-10-31 10:30:33 +0100

    fix bug if para_best is None

--- gradient_free_optimizers/print_info.py
+++ gradient_free_optimizers/print_info.py
@@ -2,6 +2,61 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
+indent = "  "
+
+
+def _print_times(eval_time, iter_time, n_iter):
+    opt_time = iter_time - eval_time
+    iterPerSec = n_iter / iter_time
+
+    print(
+        indent,
+        "Evaluation time   :",
+        eval_time,
+        "sec",
+        indent,
+        "[{} %]".format(round(eval_time / iter_time * 100, 2)),
+    )
+    print(
+        indent,
+        "Optimization time :",
+        opt_time,
+        "sec",
+        indent,
+        "[{} %]".format(round(opt_time / iter_time * 100, 2)),
+    )
+    if iterPerSec >= 1:
+        print(
+            indent,
+            "Iteration time    :",
+            iter_time,
+            "sec",
+            indent,
+            "[{} iter/sec]".format(round(iterPerSec, 2)),
+        )
+    else:
+        secPerIter = iter_time / n_iter
+        print(
+            indent,
+            "Iteration time    :",
+            iter_time,
+            "sec",
+            indent,
+            "[{} sec/iter]".format(round(secPerIter, 2)),
+        )
+
+
+def _print_results(objective_function, score_best, para_best):
+    print("\nResults: '{}'".format(objective_function.__name__), " ")
+    if para_best is None:
+        print(indent, "Best score:", score_best, " ")
+        print(indent, "Best parameter:", para_best, " ")
+    else:
+        print(indent, "Best score:", score_best, " ")
+        print(indent, "Best parameter:")
+        for key in para_best.keys():
+            print(indent, indent, "'{}'".format(key), para_best[key], " ")
+
 
 def print_info(
     verbosity,
@@ -12,51 +67,10 @@ def print_info(
     iter_time,
     n_iter,
 ):
-    indent = "  "
 
     if verbosity["print_results"] is True:
-        print("\nResults: '{}'".format(objective_function.__name__), " ")
-        print(indent, "Best score:", score_best, " ")
-        print(indent, "Best parameter:")
-        for key in para_best.keys():
-            print(indent, indent, "'{}'".format(key), para_best[key], " ")
+        _print_results(objective_function, score_best, para_best)
 
     if verbosity["print_times"] is True:
-        opt_time = iter_time - eval_time
-        iterPerSec = n_iter / iter_time
+        _print_times(eval_time, iter_time, n_iter)
 
-        print(
-            indent,
-            "Evaluation time   :",
-            eval_time,
-            "sec",
-            indent,
-            "[{} %]".format(round(eval_time / iter_time * 100, 2)),
-        )
-        print(
-            indent,
-            "Optimization time :",
-            opt_time,
-            "sec",
-            indent,
-            "[{} %]".format(round(opt_time / iter_time * 100, 2)),
-        )
-        if iterPerSec >= 1:
-            print(
-                indent,
-                "Iteration time    :",
-                iter_time,
-                "sec",
-                indent,
-                "[{} iter/sec]".format(round(iterPerSec, 2)),
-            )
-        else:
-            secPerIter = iter_time / n_iter
-            print(
-                indent,
-                "Iteration time    :",
-                iter_time,
-                "sec",
-                indent,
-                "[{} sec/iter]".format(round(secPerIter, 2)),
-            )
