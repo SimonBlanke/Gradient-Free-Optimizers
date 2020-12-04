ed313708cc1e16e030485acca5956355a63ae8c3
SimonBlanke <simonblanke@hotmail.de>
2020-12-04 16:34:17 +0100

    add scripts to create convergence plots

--- docs/plot_convergence/create_convergence_data.py
+++ docs/plot_convergence/create_convergence_data.py
@@ -1,7 +1,7 @@
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
-
+from scipy.interpolate import interp1d
 from gradient_free_optimizers import (
     HillClimbingOptimizer,
     StochasticHillClimbingOptimizer,
@@ -16,13 +16,26 @@ from gradient_free_optimizers import (
     BayesianOptimizer,
     TreeStructuredParzenEstimators,
     DecisionTreeOptimizer,
+    EnsembleOptimizer,
 )
+
+n_inits = 2
 
 
 optimizer_dict = {
     "HillClimbing": HillClimbingOptimizer,
-    "StochasticHillClimbingOptimizer": StochasticHillClimbingOptimizer,
+    "StochasticHillClimbing": StochasticHillClimbingOptimizer,
 }
+
+"""
+
+
+x = np.linspace(0, 10, num=11, endpoint=True)
+y = np.cos(-(x ** 2) / 9.0)
+f2 = interp1d(x, y, kind="cubic")
+
+print("f2", f2)
+"""
 
 
 def create_convergence_data(optimizer_key):
@@ -33,7 +46,7 @@ def create_convergence_data(optimizer_key):
     search_space = {"x1": np.arange(-100, 101, 1)}
     initialize = {"vertices": 2}
 
-    n_opts = 30
+    n_opts = 33
     n_iter = 100
 
     scores_list = []
@@ -48,13 +61,25 @@ def create_convergence_data(optimizer_key):
             initialize=initialize,
         )
 
-        scores_list.append(opt.results["score"])
+        best_scores = opt.p_bar.convergence_data
+        scores_list.append(best_scores)
 
-    convergence_data = pd.concat(scores_list, axis=1)
+    scores_np = np.array(scores_list).T
+
+    scores_mean = scores_np.mean(axis=1)
+    scores_std = scores_np.std(axis=1)
+
+    results = np.array([scores_mean, scores_std]).T
+
+    convergence_data = pd.DataFrame(
+        results, columns=["scores_mean", "scores_std"],
+    )
+
     convergence_data.to_csv(
-        "./data/" + optimizer_key + "_convergence_data", index=False
+        "./data/" + optimizer_key + "_convergence_data.csv", index=False
     )
 
 
 for opt_key in optimizer_dict.keys():
     create_convergence_data(opt_key)
+
