e97940fb0447eae6905bf0a972663757ab4d7839
Simon Blanke <simon.blanke@yahoo.com>
2021-02-05 10:04:22 +0100

    change assert to account for arrays, dataframes and other data types

--- tests/test_optimizers/test_parameter/_base_para_test.py
+++ tests/test_optimizers/test_parameter/_base_para_test.py
@@ -16,10 +16,13 @@ search_space = {"x1": np.arange(-100, 101, 1)}
 def _base_para_test_func(opt_para, optimizer):
     opt = optimizer(search_space, initialize={"vertices": 1}, **opt_para)
     opt.search(
-        objective_function, n_iter=30, memory=False, verbosity=False,
+        objective_function,
+        n_iter=30,
+        memory=False,
+        verbosity=False,
     )
 
     para_key = list(opt_para.keys())[0]
     para_value = getattr(opt, para_key)
 
-    assert para_value == opt_para[para_key]
+    assert para_value is opt_para[para_key]
