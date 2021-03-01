d060d2be6b72436efba31dc63bb6600ec5323e5b
Simon Blanke <simon.blanke@yahoo.com>
2021-03-01 08:29:28 +0100

    reformating

--- gradient_free_optimizers/optimizers/random/random_search.py
+++ gradient_free_optimizers/optimizers/random/random_search.py
@@ -9,11 +9,12 @@ from ...search import Search
 
 class RandomSearchOptimizer(BaseOptimizer, Search):
     def __init__(
-        self, search_space, initialize={"grid": 4, "random": 2, "vertices": 4},
+        self,
+        search_space,
+        initialize={"grid": 4, "random": 2, "vertices": 4},
     ):
         super().__init__(search_space, initialize)
 
     @BaseOptimizer.track_nth_iter
     def iterate(self):
         return self.move_random()
-
