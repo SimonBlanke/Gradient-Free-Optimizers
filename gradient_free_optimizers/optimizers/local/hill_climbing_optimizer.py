f218038a150b6498fc6cb7db10bbcc830c2c33da
Simon Blanke <simon.blanke@yahoo.com>
2021-03-03 07:49:19 +0100

    refactoring

--- gradient_free_optimizers/optimizers/local/hill_climbing_optimizer.py
+++ gradient_free_optimizers/optimizers/local/hill_climbing_optimizer.py
@@ -42,12 +42,8 @@ class HillClimbingOptimizer(BaseOptimizer, Search):
     def _move_climb(self, pos, epsilon_mod=1):
         sigma = self.conv.max_positions * self.epsilon * epsilon_mod
         pos_normal = dist_dict[self.distribution](pos, sigma, pos.shape)
-        pos_new_int = np.rint(pos_normal)
 
-        n_zeros = [0] * len(self.conv.max_positions)
-        pos_new = np.clip(pos_new_int, n_zeros, self.conv.max_positions)
-
-        return pos_new.astype(int)
+        return self.conv2pos(pos_normal)
 
     @BaseOptimizer.track_nth_iter
     @BaseOptimizer.random_restart
