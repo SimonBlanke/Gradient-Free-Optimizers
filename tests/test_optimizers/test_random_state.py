64c5cbd385eb0094a3696e79a6d5bca8b2e764a2
Simon Blanke <simon.blanke@yahoo.com>
2021-08-22 06:55:52 +0200

    more reliable random state tests

--- tests/test_optimizers/test_random_state.py
+++ tests/test_optimizers/test_random_state.py
@@ -5,86 +5,111 @@ import pandas as pd
 
 
 from ._parametrize import optimizers
+from surfaces.test_functions import AckleyFunction
+
+
+ackkley_function = AckleyFunction()
 
 
 def objective_function(para):
-    score = -(para["x1"] * para["x1"] + para["x2"] * para["x2"])
+    score = -(para["x0"] * para["x0"] + para["x1"] * para["x1"])
     return score
 
 
 search_space = {
-    "x1": np.arange(-20, 200, 1),
-    "x2": np.arange(-20, 200, 1),
+    "x0": np.arange(-75, 100, 1),
+    "x1": np.arange(-100, 75, 1),
 }
 
-err = 0.001
+err = 0.000001
 
-n_iter = 5
+n_iter = 10
+n_random = 2
+
+n_last = n_iter - n_random
 
 
 @pytest.mark.parametrize(*optimizers)
 def test_random_state_0(Optimizer):
-    opt0 = Optimizer(search_space, initialize={"random": 1})
+    opt0 = Optimizer(search_space, initialize={"random": n_random})
     opt0.search(
-        objective_function,
+        ackkley_function,
         n_iter=n_iter,
         random_state=1,
     )
 
-    opt1 = Optimizer(search_space, initialize={"random": 1})
+    opt1 = Optimizer(search_space, initialize={"random": n_random})
     opt1.search(
-        objective_function,
+        ackkley_function,
         n_iter=n_iter,
         random_state=1,
     )
 
-    assert abs(opt0.best_score - opt1.best_score) < err
+    n_last_scores0 = list(opt0.results["score"].values)[-n_last:]
+    n_last_scores1 = list(opt1.results["score"].values)[-n_last:]
+
+    assert abs(np.sum(n_last_scores0) - np.sum(n_last_scores1)) < err
 
 
 @pytest.mark.parametrize(*optimizers)
 def test_random_state_1(Optimizer):
-    opt0 = Optimizer(search_space, initialize={"random": 1})
+    opt0 = Optimizer(search_space, initialize={"random": n_random})
     opt0.search(
-        objective_function,
+        ackkley_function,
         n_iter=n_iter,
         random_state=10,
     )
 
-    opt1 = Optimizer(search_space, initialize={"random": 1})
+    opt1 = Optimizer(search_space, initialize={"random": n_random})
     opt1.search(
-        objective_function,
+        ackkley_function,
         n_iter=n_iter,
         random_state=10,
     )
 
-    assert abs(opt0.best_score - opt1.best_score) < err
+    n_last_scores0 = list(opt0.results["score"].values)[-n_last:]
+    n_last_scores1 = list(opt1.results["score"].values)[-n_last:]
+
+    assert abs(np.sum(n_last_scores0) - np.sum(n_last_scores1)) < err
 
 
 @pytest.mark.parametrize(*optimizers)
 def test_random_state_2(Optimizer):
-    opt0 = Optimizer(search_space, initialize={"random": 1})
+    opt0 = Optimizer(search_space, initialize={"random": n_random})
     opt0.search(
-        objective_function,
+        ackkley_function,
         n_iter=n_iter,
         random_state=1,
     )
 
-    opt1 = Optimizer(search_space, initialize={"random": 1})
+    opt1 = Optimizer(search_space, initialize={"random": n_random})
     opt1.search(
-        objective_function,
+        ackkley_function,
         n_iter=n_iter,
         random_state=10,
     )
 
-    assert abs(opt0.best_score - opt1.best_score) > err
+    print("\n opt0.results \n", opt0.results)
+    print("\n opt1.results \n", opt1.results)
+
+    n_last_scores0 = list(opt0.results["score"].values)[-n_last:]
+    n_last_scores1 = list(opt1.results["score"].values)[-n_last:]
+
+    assert abs(np.sum(n_last_scores0) - np.sum(n_last_scores1)) > err
 
 
 @pytest.mark.parametrize(*optimizers)
 def test_no_random_state_0(Optimizer):
-    opt0 = Optimizer(search_space, initialize={"random": 1})
-    opt0.search(objective_function, n_iter=n_iter)
+    opt0 = Optimizer(search_space, initialize={"random": n_random})
+    opt0.search(ackkley_function, n_iter=n_iter)
 
-    opt1 = Optimizer(search_space, initialize={"random": 1})
-    opt1.search(objective_function, n_iter=n_iter)
+    opt1 = Optimizer(search_space, initialize={"random": n_random})
+    opt1.search(ackkley_function, n_iter=n_iter)
 
-    assert abs(opt0.best_score - opt1.best_score) > err
+    print("\n opt0.results \n", opt0.results)
+    print("\n opt1.results \n", opt1.results)
+
+    n_last_scores0 = list(opt0.results["score"].values)[-n_last:]
+    n_last_scores1 = list(opt1.results["score"].values)[-n_last:]
+
+    assert abs(np.sum(n_last_scores0) - np.sum(n_last_scores1)) > err
