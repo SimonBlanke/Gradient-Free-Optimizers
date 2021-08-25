ea43a1f5d1900cdc167b9edd48c0929346f38540
Simon Blanke <simon.blanke@yahoo.com>
2021-08-25 19:41:43 +0200

    return random pos if y_sample is empty

--- gradient_free_optimizers/optimizers/sequence_model/exp_imp_based_opt.py
+++ gradient_free_optimizers/optimizers/sequence_model/exp_imp_based_opt.py
@@ -64,6 +64,9 @@ class ExpectedImprovementBasedOptimization(SMBO):
         X_sample = np.array(self.X_sample)
         Y_sample = np.array(self.Y_sample)
 
+        if len(Y_sample) == 0:
+            return self.move_random()
+
         Y_sample = normalize(Y_sample).reshape(-1, 1)
         self.regr.fit(X_sample, Y_sample)
 
