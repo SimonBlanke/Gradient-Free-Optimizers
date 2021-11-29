4ef61a79fbefdd055d0b227fe8c20b5c05134b2c
Thomas Gak Deluen <tgdn45@gmail.com>
2021-11-29 17:13:40 +0100

    Step size update

--- gradient_free_optimizers/optimizers/grid/grid_search.py
+++ gradient_free_optimizers/optimizers/grid/grid_search.py
@@ -27,7 +27,8 @@ class GridSearchOptimizer(BaseOptimizer, Search):
     def get_direction(self):
         """
         Aim here is to generate a prime number of the search space size we call our direction.
-        As direction is prime with the search space size, we know it is a generator of Z/(search_space_size*Z).
+        As direction is prime with the search space size, we know it is
+        a generator of Z/(search_space_size*Z).
         """
         n_dims = self.conv.n_dimensions
         search_space_size = self.conv.search_space_size
@@ -52,32 +53,45 @@ class GridSearchOptimizer(BaseOptimizer, Search):
         new_pos = []
         dim_sizes = self.conv.dim_sizes
         pointer = self.high_dim_pointer
-        
-        # The coordinate of our new position for each dimension is the quotient of the pointer by the product of remaining dimensions
+
+        # The coordinate of our new position for each dimension is
+        # the quotient of the pointer by the product of remaining dimensions
         # Describes a bijection from Z/search_space_size*Z -> (Z/dim_1*Z)x...x(Z/dim_n*Z)
         for dim in range(len(dim_sizes) - 1):
-            new_pos.append(pointer // np.prod(dim_sizes[dim + 1 :]) % dim_sizes[dim])
+            new_pos.append(
+                pointer // np.prod(dim_sizes[dim + 1 :]) % dim_sizes[dim]
+            )
             pointer = pointer % np.prod(dim_sizes[dim + 1 :])
         new_pos.append(pointer)
         return np.array(new_pos)
 
     @BaseOptimizer.track_nth_iter
     def iterate(self):
+        # If this is the first iteration:
+        # Generate the direction and return initial_position
         if self.direction is None:
-            # If this is the first iteration, generate the direction and return initial_position attribute
             self.direction = self.get_direction()
             return self.initial_position
+
+        # If this is not the first iteration:
+        # Update high_dim_pointer by taking a step of size step_size * direction.
+
+        # Multiple passes are needed in order to observe the entire search space
+        # depending on the step_size parameter.
+        _, current_pass = self.nth_iter, self.high_dim_pointer % self.step_size
+        current_pass_finished = (
+            (self.nth_iter + 1) * self.step_size // self.conv.search_space_size
+            > self.nth_iter * self.step_size // self.conv.search_space_size
+        )
+        # Begin the next pass if current is finished.
+        if current_pass_finished:
+            self.high_dim_pointer = current_pass + 1
         else:
-            # Else, update high_dim_pointer by taking a step of size step_size * direction
-            # Multiple passes are needed in order to observe the entire search space because of the step_size argument
-            iter_, pass_ = self.nth_iter, self.high_dim_pointer % self.step_size
-            # If current pass is finished, begin the next one
-            if (self.nth_iter+1) * self.step_size // self.conv.search_space_size > self.nth_iter * self.step_size // self.conv.search_space_size:
-                self.high_dim_pointer = pass_ + 1
-            else:
-                # Else, update pointer in Z/(search_space_size*Z) using the prime step direction and step_size
-                self.high_dim_pointer = (
-                    self.high_dim_pointer + self.step_size * self.direction
-                ) % self.conv.search_space_size
-            # Compute corresponding position in our search space
-            return self.grid_move()
+            # Otherwise update pointer in Z/(search_space_size*Z)
+            # using the prime step direction and step_size.
+            self.high_dim_pointer = (
+                self.high_dim_pointer + self.step_size * self.direction
+            ) % self.conv.search_space_size
+
+        # Compute corresponding position in our search space.
+        return self.grid_move()
