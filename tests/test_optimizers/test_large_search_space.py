3f22464c05479353554e73b4a8315cf271e2d1eb
Simon Blanke <simon.blanke@yahoo.com>
2022-10-22 12:40:42 +0200

    test large search-space for smbo

--- tests/test_optimizers/test_large_search_space.py
+++ tests/test_optimizers/test_large_search_space.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from ._parametrize import optimizers_noSBOM
+from ._parametrize import optimizers_noSBOM, optimizers_SBOM
 
 
 def objective_function(para):
@@ -43,3 +43,15 @@ def test_large_search_space_2(Optimizer):
 
     opt = Optimizer(search_space, initialize={"random": 34, "vertices": 34, "grid": 34})
     opt.search(objective_function, n_iter=1000, verbosity=False)
+
+
+@pytest.mark.parametrize(*optimizers_SBOM)
+def test_large_search_space_3(Optimizer):
+
+    search_space = {}
+    for i in range(31):
+        key = "x" + str(i)
+        search_space[key] = np.arange(0, 100)
+
+    opt = Optimizer(search_space, initialize={"random": 1})
+    opt.search(objective_function, n_iter=2, verbosity=False)
