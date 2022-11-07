8f65e0e4c51455465f2fed43998e6e7b56d07fe5
Simon Blanke <simon.blanke@yahoo.com>
2022-11-07 19:26:57 +0100

    remove support for python 3.11 for now (api-change in random-module ...)

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Information Analysis",
