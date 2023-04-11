bdbe4f808168f7be4d4f4e45edd2ba494e7c74ab
Simon Blanke <simon.blanke@yahoo.com>
2023-04-11 09:14:56 +0200

    add explanations to example

--- examples/constrained_optimization.py
+++ examples/constrained_optimization.py
@@ -14,15 +14,19 @@ search_space = {
 
 
 def constraint_1(para):
+    # only values in 'x1' higher than -5 are valid
     return para["x1"] > -5
 
 
+# put one or more constraints inside a list
 constraints_list = [constraint_1]
 
 
+# pass list of constraints to the optimizer
 opt = RandomSearchOptimizer(search_space, constraints=constraints_list)
 opt.search(convex_function, n_iter=50)
 
 search_data = opt.search_data
 
+# the search-data does not contain any samples where x1 is equal or below -5
 print("\n search_data \n", search_data, "\n")
