ac7213455b6564cbaa085885543a9ce255966bac
Simon Blanke <simon.blanke@yahoo.com>
2024-07-13 16:36:08 +0200

    add crossover-rate parameter to discr. recomb. method

--- gradient_free_optimizers/optimizers/pop_opt/_evolutionary_algorithm.py
+++ gradient_free_optimizers/optimizers/pop_opt/_evolutionary_algorithm.py
@@ -12,15 +12,13 @@ class EvolutionaryAlgorithmOptimizer(BasePopulationOptimizer):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    def discrete_recombination(self, parent_pos_l):
-        n_arrays = len(parent_pos_l)
+    def discrete_recombination(self, parent_pos_l, crossover_rates=None):
+        n_parents = len(parent_pos_l)
         size = parent_pos_l[0].size
 
-        if random.choice([True, False]):
-            choice = [True, False]
-        else:
-            choice = [False, True]
-        if size > 2:
-            add_choice = np.random.randint(n_arrays, size=size - 2).astype(bool)
-            choice += list(add_choice)
+        choice = []
+        for _ in range(size):
+            choices = list(range(n_parents))
+            choice.append(np.random.choice(choices, p=crossover_rates))
+
         return np.choose(choice, parent_pos_l)
