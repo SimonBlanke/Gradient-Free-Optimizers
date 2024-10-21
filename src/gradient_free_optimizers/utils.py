e5cd7a87fb2caca43107279f1e1e77acea5c74dc
Simon Blanke <simon.blanke@yahoo.com>
2024-10-21 21:08:12 +0200

    refactor move_random method

--- src/gradient_free_optimizers/utils.py
+++ src/gradient_free_optimizers/utils.py
@@ -15,7 +15,9 @@ def set_random_seed(nth_process, random_state):
         nth_process = 0
 
     if random_state is None:
-        random_state = np.random.randint(0, high=2**31 - 2, dtype=np.int64).item()
+        random_state = np.random.randint(
+            0, high=2**31 - 2, dtype=np.int64
+        ).item()
 
     random.seed(random_state + nth_process)
     np.random.seed(random_state + nth_process)
@@ -24,9 +26,15 @@ def set_random_seed(nth_process, random_state):
 
 
 def move_random(ss_positions):
-    position = []
-    for search_space_pos in ss_positions:
-        pos_ = random.choice(search_space_pos)
-        position.append(pos_)
+    """
+    Selects a random element from each sublist in ss_positions and returns them as a NumPy array.
 
-    return np.array(position)
+    Args:
+    ss_positions (list): A list of lists representing search spaces of possible positions.
+
+    Returns:
+    np.array: NumPy array containing one randomly selected element from each sublist in ss_positions.
+    """
+    return np.array(
+        [random.choice(search_space_pos) for search_space_pos in ss_positions]
+    )
