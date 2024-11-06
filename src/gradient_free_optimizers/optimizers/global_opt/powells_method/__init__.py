a9f809a8f5e1f3278c5c3bbd1f3cb4947c9b55cd
Simon Blanke <simon.blanke@yahoo.com>
2024-11-06 20:01:21 +0100

    add new module for powells search

---
+++ src/gradient_free_optimizers/optimizers/global_opt/powells_method/__init__.py
@@ -0,0 +1,11 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+from .powells_method import PowellsMethod
+
+
+__all__ = [
+    "PowellsMethod",
+]
---
+++ src/gradient_free_optimizers/optimizers/global_opt/powells_method/direction.py
@@ -0,0 +1,8 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+class Direction:
+    def __init__(self) -> None:
+        pass
