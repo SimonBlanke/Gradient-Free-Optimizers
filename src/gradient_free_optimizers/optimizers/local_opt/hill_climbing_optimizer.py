9406fcb2acc358c7aaf68e8b300b776f9cfc9184
Simon Blanke <simon.blanke@yahoo.com>
2024-11-27 15:23:17 +0100

    remove *args, **kwargs

--- src/gradient_free_optimizers/optimizers/local_opt/hill_climbing_optimizer.py
+++ src/gradient_free_optimizers/optimizers/local_opt/hill_climbing_optimizer.py
@@ -32,13 +32,25 @@ class HillClimbingOptimizer(BaseOptimizer):
 
     def __init__(
         self,
-        *args,
+        search_space,
+        initialize={"grid": 4, "random": 2, "vertices": 4},
+        constraints=[],
+        random_state=None,
+        rand_rest_p=0,
+        nth_process=None,
         epsilon=0.03,
         distribution="normal",
         n_neighbours=3,
-        **kwargs
     ):
-        super().__init__(*args, **kwargs)
+
+        super().__init__(
+            search_space=search_space,
+            initialize=initialize,
+            constraints=constraints,
+            random_state=random_state,
+            rand_rest_p=rand_rest_p,
+            nth_process=nth_process,
+        )
         self.epsilon = epsilon
         self.distribution = distribution
         self.n_neighbours = n_neighbours
