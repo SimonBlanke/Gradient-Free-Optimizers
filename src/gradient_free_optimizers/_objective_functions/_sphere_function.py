4c9d2c31fcaa7ccf0ba3fc62310896b945542925
Simon Blanke <simon.blanke@yahoo.com>
2025-06-28 09:38:18 +0200

    add sphere toy function

---
+++ src/gradient_free_optimizers/_objective_functions/_sphere_function.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class SphereFunction:
+    def __init__(self, n_dim, A=1):
+        self.n_dim = n_dim
+        self.A = A
+
+    def objective_function(self, para):
+        loss = 0
+        for dim in range(self.n_dim):
+            dim_str = "x" + str(dim)
+            x = para[dim_str]
+
+            loss += self.A * x * x
+
+        return loss
+
+    @property
+    def search_space(self):
+        return {"x" + str(idx): np.arange(-8, 8, 0.1) for idx in range(self.n_dim)}
