3b438639fec4f6bf92bcf873fb6eced1fd51007b
Simon Blanke <simon.blanke@yahoo.com>
2025-07-09 17:16:55 +0200

    add gpr fit-method

--- src/gradient_free_optimizers/_estimators/_gaussian_process_regressor.py
+++ src/gradient_free_optimizers/_estimators/_gaussian_process_regressor.py
@@ -28,3 +28,73 @@ class GaussianProcessRegressor:
         self.y = None
         self.L = None
         self.alpha = None
+
+    def fit(self, X, y):
+        """
+        Fit the GP hyper-parameters and pre-compute quantities needed
+        for prediction.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n, d)
+            Training inputs.
+        y : ndarray, shape (n,)
+            Training targets.
+
+        Returns
+        -------
+        self : fitted estimator.
+        """
+        self.X = np.asarray(X, dtype=float)
+        self.y = np.asarray(y, dtype=float).ravel()
+
+        if self.optimize:
+            self.theta = self._optimise_theta()
+
+        K = self.kernel(self.X, self.X, self.theta)
+        sigma_n2 = np.exp(2 * self.theta[-1])
+        K += sigma_n2 * np.eye(len(self.X))
+
+        self.L = cholesky(K, lower=True, check_finite=False)
+        self.alpha = cho_solve((self.L, True), self.y, check_finite=False)
+
+        return self
+
+    def _log_marginal_likelihood(self, theta):
+        K = self.kernel(self.X, self.X, theta)
+        sigma_n2 = np.exp(2 * theta[-1])
+        K += sigma_n2 * np.eye(len(self.X))
+
+        L = cholesky(K, lower=True, check_finite=False)
+        alpha = cho_solve((L, True), self.y, check_finite=False)
+
+        lml = -0.5 * self.y @ alpha
+        lml -= np.sum(np.log(np.diag(L)))
+        lml -= 0.5 * len(self.X) * np.log(2 * np.pi)
+
+        eps = 1e-5
+        grad = np.empty_like(theta)
+        for i in range(len(theta)):
+            theta_eps = theta.copy()
+            theta_eps[i] += eps
+            K_eps = self.kernel(self.X, self.X, theta_eps)
+            K_eps += np.exp(2 * theta_eps[-1]) * np.eye(len(self.X))
+            L_eps = cholesky(K_eps, lower=True, check_finite=False)
+            alpha_eps = cho_solve((L_eps, True), self.y, check_finite=False)
+            lml_eps = -0.5 * self.y @ alpha_eps
+            lml_eps -= np.sum(np.log(np.diag(L_eps)))
+            lml_eps -= 0.5 * len(self.X) * np.log(2 * np.pi)
+            grad[i] = (lml_eps - lml) / eps
+
+        return -lml, -grad
+
+    def _optimise_theta(self):
+        """Maximise log-marginal likelihood starting from self.theta."""
+        res = minimize(
+            fun=lambda th: self._log_marginal_likelihood(th),
+            x0=self.theta,
+            jac=True,
+            method="L-BFGS-B",
+            options=dict(maxiter=100, ftol=1e-6),
+        )
+        return res.x
