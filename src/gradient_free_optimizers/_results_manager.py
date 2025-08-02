cd4a8535515120b66e2c2813521581051ec2c335
Simon Blanke <simon.blanke@yahoo.com>
2025-08-02 13:58:18 +0200

    refactor ResultsManager class

--- src/gradient_free_optimizers/_results_manager.py
+++ src/gradient_free_optimizers/_results_manager.py
@@ -1,43 +1,19 @@
-# Author: Simon Blanke
-# Email: simon.blanke@yahoo.com
-# License: MIT License
-
 import pandas as pd
+from collections.abc import Sequence
+
+from ._trial_result import TrialResult
 
 
 class ResultsManager:
     def __init__(self):
-        super().__init__()
-        self.conv = None
+        self._results: list[TrialResult] = []
 
-        self.results_list = []
-
-    def _obj_func_results(self, objective_function, para):
-        results = objective_function(para)
-
-        if isinstance(results, tuple):
-            score = results[0]
-            results_dict = results[1]
-        else:
-            score = results
-            results_dict = {}
-
-        results_dict["score"] = score
-
-        return results_dict
-
-    def score(self, objective_function):
-        def _wrapper(pos):
-            value = self.conv.position2value(pos)
-            para = self.conv.value2para(value)
-            results_dict = self._obj_func_results(objective_function, para)
-
-            self.results_list.append({**results_dict, **para})
-
-            return results_dict["score"]
-
-        return _wrapper
+    def add(self, result: TrialResult) -> None:
+        self._results.append(result)
 
     @property
-    def search_data(self):
-        return pd.DataFrame(self.results_list)
+    def dataframe(self) -> pd.DataFrame:
+        return pd.DataFrame(r.asdict() for r in self._results)
+
+    def best(self) -> TrialResult | None:
+        return max(self._results, key=lambda r: r.score, default=None)
