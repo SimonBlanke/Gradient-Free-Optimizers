1b56976c93ff1b36cb375cffc1d1a43904d2d27d
Simon Blanke <simon.blanke@yahoo.com>
2026-01-02 19:32:11 +0100

    add new estimators to module

--- src/gradient_free_optimizers/_estimators/__init__.py
+++ src/gradient_free_optimizers/_estimators/__init__.py
@@ -10,3 +10,8 @@ These are not part of the public API and should only be used internally.
 
 from ._gaussian_process_regressor import GaussianProcessRegressor
 from ._kernel_density_estimator import KernelDensityEstimator
+from ._decision_tree_regressor import DecisionTreeRegressor
+from ._random_forest_regressor import RandomForestRegressor
+from ._extra_trees_regressor import ExtraTreesRegressor
+from ._gradient_boosting_regressor import GradientBoostingRegressor
+from ._bayesian_ridge import BayesianRidge
