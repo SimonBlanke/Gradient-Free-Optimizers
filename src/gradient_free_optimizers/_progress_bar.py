e07dafd1ba385dda300b812bd0b6d0a3649554a3
Simon Blanke <simon.blanke@yahoo.com>
2026-01-02 20:12:38 +0100

    add 'SimpleProgressBar' class

--- src/gradient_free_optimizers/_progress_bar.py
+++ src/gradient_free_optimizers/_progress_bar.py
@@ -3,7 +3,59 @@
 # License: MIT License
 
 import numpy as np
-from tqdm import tqdm
+
+try:
+    from tqdm import tqdm
+
+    TQDM_AVAILABLE = True
+except ImportError:
+    TQDM_AVAILABLE = False
+
+
+class SimpleProgressBar:
+    """Fallback progress bar using print statements when tqdm is unavailable."""
+
+    def __init__(self, total, desc="", position=None, leave=False):
+        self.total = total
+        self.desc = desc
+        self.n = 0
+        self.postfix = {}
+        self._last_printed = -1
+
+    def update(self, n=1):
+        self.n += n
+        percent = int(self.n / self.total * 100)
+        if percent >= self._last_printed + 10 or self.n == self.total:
+            self._last_printed = percent
+            self._print_progress()
+
+    def set_postfix(self, **kwargs):
+        self.postfix = kwargs
+
+    def _print_progress(self):
+        bar_len = 20
+        filled = int(bar_len * self.n / self.total)
+        bar = "#" * filled + "-" * (bar_len - filled)
+
+        postfix_str = ""
+        if self.postfix:
+            postfix_str = " | " + ", ".join(
+                f"{k}={v}" for k, v in self.postfix.items()
+            )
+
+        desc_str = f"{self.desc}: " if self.desc else ""
+        print(
+            f"\r{desc_str}[{bar}] {self.n}/{self.total}{postfix_str}",
+            end="",
+            flush=True,
+        )
+
+        if self.n == self.total:
+            print()
+
+    def close(self):
+        if self.n < self.total:
+            print()
 
 
 class ProgressBarBase:
@@ -62,7 +114,10 @@ class ProgressBarLVL0(ProgressBarBase):
 class ProgressBarLVL1(ProgressBarBase):
     def __init__(self, nth_process, n_iter, objective_function):
         super().__init__(nth_process, n_iter, objective_function)
-        self._tqdm = tqdm(**self._tqdm_dict(nth_process, n_iter, objective_function))
+        progress_bar_class = tqdm if TQDM_AVAILABLE else SimpleProgressBar
+        self._tqdm = progress_bar_class(
+            **self._tqdm_dict(nth_process, n_iter, objective_function)
+        )
 
     def update(self, score_new, pos_new, nth_iter):
         self.n_iter_current = nth_iter
