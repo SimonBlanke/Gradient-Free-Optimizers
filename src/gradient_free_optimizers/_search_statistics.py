87d17e08744014bf42df1bbc040ce37adc17afb1
Simon Blanke <simon.blanke@yahoo.com>
2026-01-08 09:19:11 +0100

    wrap init_stats method

--- src/gradient_free_optimizers/_search_statistics.py
+++ src/gradient_free_optimizers/_search_statistics.py
@@ -2,6 +2,8 @@
 # Email: simon.blanke@yahoo.com
 # License: MIT License
 
+from functools import wraps
+
 
 class SearchStatistics:
     def __init__(self):
@@ -13,6 +15,7 @@ class SearchStatistics:
         self.n_iter_total = 0
 
     def init_stats(func):
+        @wraps(func)
         def wrapper(self, *args, **kwargs):
             self.n_init_search = 0
             self.n_iter_search = 0
